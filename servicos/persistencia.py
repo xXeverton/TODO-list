0ba778dcc0c276e16f63ab0a385f3242de6db2b2
mateusmansour <64480742+mateusmansour@users.noreply.github.com>
2025-06-22 15:51:38 -0300

    pasta de dados

--- servicos/persistencia.py
+++ servicos/persistencia.py
@@ -4,16 +4,18 @@ import json, os
 __all__ = ["salva_estado", "carrega_estado"]
 
 def salva_estado(data, filename):
+    path = "data/"+filename
     if type(data) is not list:
         return False
-    with open(filename,"w") as f:
+    with open(path,"w") as f:
         json.dump(data, f, indent=2)
 
     return True
 
 def carrega_estado(filename):
-    if os.path.exists(filename):
-        with open(filename, "r") as f:
+    path = "data/"+filename
+    if os.path.exists(path):
+        with open(path, "r") as f:
             data = json.load(f)
         return data
     else:
